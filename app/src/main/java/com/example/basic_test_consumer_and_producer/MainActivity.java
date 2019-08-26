7232fbd06e5cac6be6baf40272377994e9196c00
Edward Lu <edwardzlu98@gmail.com>
2019-08-26 14:38:24 -0400

    Initial commit

---
+++ app/src/main/java/com/example/basic_test_consumer_and_producer/MainActivity.java
@@ -0,0 +1,129 @@
+package com.example.basic_test_consumer_and_producer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import net.named_data.jndn.Data;
+import net.named_data.jndn.Interest;
+import net.named_data.jndn.Name;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private static final int PRODUCER = 0, CONSUMER = 1;
+    private static int mode;
+
+    public static final int MSG_DATA_RECEIVED = 0;
+
+    public static final String STREAM_NAME = "/test";
+
+    MainActivity mainActivity_;
+    Handler handler_;
+    NetworkThreadConsumer networkThreadConsumer_;
+    NetworkThreadProducer networkThreadProducer_;
+
+    int numOutstandingInterests_ = 0;
+    long currentSegNum_ = 0;
+    Name currentStreamName_;
+
+    boolean start_ = false;
+
+    Button chooseModeButton_;
+    Button startButton_;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mainActivity_ = this;
+
+        currentStreamName_ = new Name(STREAM_NAME);
+
+        chooseModeButton_ = (Button) findViewById(R.id.choose_mode_button);
+        chooseModeButton_.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (chooseModeButton_.getText().toString().equals(getString(R.string.producer_mode))) {
+                    chooseModeButton_.setText(getString(R.string.consumer_mode));
+                }
+                else {
+                    chooseModeButton_.setText(getString(R.string.producer_mode));
+                }
+            }
+        });
+
+        startButton_ = (Button) findViewById(R.id.start_button);
+        startButton_.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseModeButton_.setEnabled(false);
+                startButton_.setEnabled(false);
+                if (chooseModeButton_.getText().toString().equals(getString(R.string.producer_mode)))
+                    mode = PRODUCER;
+                else
+                    mode = CONSUMER;
+
+                switch (mode) {
+                    case PRODUCER:
+                        networkThreadProducer_ = new NetworkThreadProducer(currentStreamName_);
+                        networkThreadProducer_.start();
+                        break;
+                    case CONSUMER:
+                        handler_ = new Handler(mainActivity_.getMainLooper()) {
+                            @Override
+                            public void handleMessage(@NonNull Message msg) {
+                                super.handleMessage(msg);
+                                switch (msg.what) {
+                                    case MSG_DATA_RECEIVED:
+                                        Data data = (Data) msg.obj;
+                                        Log.d(TAG, System.currentTimeMillis() + ": " +
+                                                "received data (" +
+                                                "name: " + data.getName().toString() +
+                                                ")");
+                                        modifyNumOutstandingInterests(-1);
+                                        break;
+                                    default:
+                                        throw new IllegalStateException();
+                                }
+                            }
+                        };
+
+                        networkThreadConsumer_ = new NetworkThreadConsumer(handler_);
+                        networkThreadConsumer_.start();
+                        while (networkThreadConsumer_.getHandler() == null) {}
+
+                        while (true) {
+                            Name interestName = new Name(currentStreamName_);
+                            Interest interest = new Interest(interestName.appendSegment(currentSegNum_));
+                            currentSegNum_++;
+                            networkThreadConsumer_.getHandler()
+                                    .obtainMessage(NetworkThreadConsumer.MSG_INTEREST_SEND_REQUEST, interest)
+                                    .sendToTarget();
+                            SystemClock.sleep(1000);
+                        }
+                }
+            }
+        });
+
+    }
+
+    public void modifyNumOutstandingInterests(int modifier) {
+        Log.d(TAG, System.currentTimeMillis() + ": " +
+                "modifying numOutstandingInterests_ (" +
+                "current value: " + numOutstandingInterests_ + ", " +
+                "modifier: " + modifier +
+                ")");
+        numOutstandingInterests_ += modifier;
+    }
+
+}
---
+++ app/src/main/java/com/example/basic_test_consumer_and_producer/NetworkThreadConsumer.java
@@ -0,0 +1,167 @@
+package com.example.basic_test_consumer_and_producer;
+
+import android.annotation.SuppressLint;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Message;
+import android.os.SystemClock;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import net.named_data.jndn.ContentType;
+import net.named_data.jndn.Data;
+import net.named_data.jndn.Face;
+import net.named_data.jndn.Interest;
+import net.named_data.jndn.Name;
+import net.named_data.jndn.OnData;
+import net.named_data.jndn.encoding.EncodingException;
+import net.named_data.jndn.security.KeyChain;
+import net.named_data.jndn.security.SecurityException;
+import net.named_data.jndn.security.identity.IdentityManager;
+import net.named_data.jndn.security.identity.MemoryIdentityStorage;
+import net.named_data.jndn.security.identity.MemoryPrivateKeyStorage;
+import net.named_data.jndn.security.policy.SelfVerifyPolicyManager;
+
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class NetworkThreadConsumer extends HandlerThread {
+
+    private final static String TAG = "NetworkThreadConsumer";
+
+    private Face face_;
+    private KeyChain keyChain_;
+
+    private long startTime_;
+    private Handler mainActivityHandler_;
+    private Handler handler_;
+    private boolean closed_ = false;
+
+    // Private constants
+    private static final int PROCESSING_INTERVAL_MS = 100;
+
+    // Messages
+    private static final int MSG_DO_SOME_WORK = 0;
+    public static final int MSG_INTEREST_SEND_REQUEST = 1;
+
+    private long getTimeSinceNetworkThreadStart() {
+        return System.currentTimeMillis() - startTime_;
+    }
+
+    public NetworkThreadConsumer(Handler mainActivityHandler) {
+        super("NetworkThreadConsumer");
+        mainActivityHandler_ = mainActivityHandler;
+    }
+
+    private void doSomeWork() {
+        if (closed_) return;
+        try {
+            face_.processEvents();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (EncodingException e) {
+            e.printStackTrace();
+        }
+
+        scheduleNextWork(SystemClock.uptimeMillis(), PROCESSING_INTERVAL_MS);
+    }
+
+    private void scheduleNextWork(long thisOperationStartTimeMs, long intervalMs) {
+        handler_.removeMessages(MSG_DO_SOME_WORK);
+        handler_.sendEmptyMessageAtTime(MSG_DO_SOME_WORK, thisOperationStartTimeMs + intervalMs);
+    }
+
+    private void sendInterest(Interest interest) {
+        Log.d(TAG, getTimeSinceNetworkThreadStart() + ": " + "send interest (name " + interest.getName().toString() + ")");
+        try {
+            face_.expressInterest(interest, new OnData() {
+                @Override
+                public void onData(Interest interest, Data data) {
+                    long satisfiedTime = System.currentTimeMillis();
+                    Log.d(TAG, getTimeSinceNetworkThreadStart() + ": " + "data received (" +
+                            "name " + data.getName().toString() + ", " +
+                            "time " + satisfiedTime + ")");
+                    mainActivityHandler_.obtainMessage(MainActivity.MSG_DATA_RECEIVED, data).sendToTarget();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void close() {
+        Log.d(TAG, getTimeSinceNetworkThreadStart() + ": " + "close called");
+        handler_.removeCallbacksAndMessages(null);
+        handler_.getLooper().quitSafely();
+        closed_ = true;
+    }
+
+    @SuppressLint("HandlerLeak")
+    @Override
+    protected void onLooperPrepared() {
+        super.onLooperPrepared();
+
+        startTime_ = System.currentTimeMillis();
+
+        // set up keychain
+        keyChain_ = configureKeyChain();
+        // set up face
+        face_ = new Face();
+        try {
+            face_.setCommandSigningInfo(keyChain_, keyChain_.getDefaultCertificateName());
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+
+        handler_ = new Handler() {
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                switch (msg.what) {
+                    case MSG_DO_SOME_WORK:
+                        doSomeWork();
+                        break;
+                    case MSG_INTEREST_SEND_REQUEST:
+                        Interest interest = (Interest) msg.obj;
+                        sendInterest(interest);
+                        break;
+                    default:
+                        throw new IllegalStateException();
+                }
+            }
+        };
+
+        doSomeWork();
+    }
+
+    // taken from https://github.com/named-data-mobile/NFD-android/blob/4a20a88fb288403c6776f81c1d117cfc7fced122/app/src/main/java/net/named_data/nfd/utils/NfdcHelper.java
+    private KeyChain configureKeyChain() {
+
+        final MemoryIdentityStorage identityStorage = new MemoryIdentityStorage();
+        final MemoryPrivateKeyStorage privateKeyStorage = new MemoryPrivateKeyStorage();
+        final KeyChain keyChain = new KeyChain(new IdentityManager(identityStorage, privateKeyStorage),
+                new SelfVerifyPolicyManager(identityStorage));
+
+        Name name = new Name("/tmp-identity");
+
+        try {
+            // create keys, certs if necessary
+            if (!identityStorage.doesIdentityExist(name)) {
+                keyChain.createIdentityAndCertificate(name);
+
+                // set default identity
+                keyChain.getIdentityManager().setDefaultIdentity(name);
+            }
+        }
+        catch (SecurityException e){
+            e.printStackTrace();
+        }
+
+        return keyChain;
+    }
+
+    public Handler getHandler() {
+        return handler_;
+    }
+
+}
